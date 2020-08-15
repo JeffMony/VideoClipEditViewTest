29292c547a166af97007846591ef43d41deacc68
JeffMony <jeffmony@163.com>
2020-08-16 00:02:15 +0800

    修改player 库

--- library/src/main/java/com/spx/library/VideoItem.kt
+++
@@ -1,3 +0,0 @@
-package com.spx.library
-
-class VideoItem(val title: String, val path: String, val durationSec: Int, val size: Long)
--- library/src/main/res/raw/def_vertext.glsl
+++
@@ -1,7 +0,0 @@
-attribute vec4 aPosition;
-attribute vec4 aTextureCoord;
-varying highp vec2 textureCoordinate;
-void main() {
-gl_Position = aPosition;
-textureCoordinate = aTextureCoord.xy;
-}
